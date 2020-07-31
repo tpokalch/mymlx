826640b393299db9b12bda8730a35c26d6baf607
Maria Mayerchyk <mariamayerchyk@maria-mayerchyks-macbook.local>
2020-07-31 17:58:33 +0300

    first

---
+++ README.md
@@ -0,0 +1,24 @@
+# RT
+This is a somewhat complex raytracing implementation. It supports  reflection, transparency, light spots, multiple light sources, complex objects, textures, CPU multi-threading, and soft shadows.
+
+
+<p align="center">
+  <img width="80%" src="https://github.com/tpokalch/RT/blob/master/screens/soft.png">
+</p>
+
+
+<p align="center">
+  <img width="80%" src="https://raw.githubusercontent.com/tpokalch/RT/master/screens/Screen%20Shot%202019-09-28%20at%205.22.04%20AM.png">
+</p>
+
+
+<p align="center">
+  <img width="80%" src="https://raw.githubusercontent.com/tpokalch/RT/master/screens/all.png">
+</p>
+
+
+Click on video below:
+
+
+[![rtdemo](https://i.imgur.com/qXSK70v.jpg)](https://www.youtube.com/watch?v=3ymJMfN1hsI&feature=youtu.be)
+
---
+++ events.c
@@ -0,0 +1,226 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpokalch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/03 16:10:51 by tpokalch          #+#    #+#             */
+/*   Updated: 2019/07/17 21:25:14 by tpokalch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+//	this function is needed to check for the direction of the plane/tri
+//	normals only once after the movement of the object, or movement of
+//	the camera
+int		campos(t_global *g)
+{
+	int i;
+
+	i = 1;
+	while (i < g->argc + 1)
+	{
+		if ((ft_strequ("plane", g->obj[i].name) || ft_strequ("tri", g->obj[i].name))  && dot(diff(*g->obj[i].ctr, *g->cam_pos), g->obj[i].base[1]) > 0)
+		{
+//			g->obj[i].base[1] = scale(-1, g->obj[i].base[1]);
+			g->obj[i].cam_pos = 1;
+			printf("we are in %d %s\n", i, g->obj[i].name);
+		}
+		else
+			g->obj[i].cam_pos = 0;
+		i++;
+	}
+	return (1);
+}
+
+int		key_press(int kk, void *param)
+{
+	t_global *g;
+
+	g = param;
+	printf("keycode is %d\n", kk);
+//	if (kk == 47)
+//	comment this to keep shot constant
+//	uncomment to keep shot only 1 frame
+//		shot.x = -WIDTH;
+//	ft_bzero((int *)g->data_ptr, g->sz_l * HEIGHT);
+	if (kk == 53)
+	{//	system("leaks -s rtv1");
+//		system("leaks -s rtv1");	
+		exit(free_hits(g));
+	}
+	else if (kk == 13 || kk == 1 || kk == 0 || kk == 2 || kk == 3 || kk == 125
+		|| kk == 126 || kk == 38 || kk == 40 || kk == 37 || kk == 34
+		|| kk == 123 || kk == 124)
+		return(move_obj(kk, g));
+	else if (kk == 16)
+		g->objn = (g->objn + 1) % (g->argc + 1);
+	else if (kk == 4 || kk == 5) //camera perspective
+	{
+		g->ray->z = fabs(g->ray->z + 10 * (2 * (kk == 4) - 1));
+		copy_tcps(g);
+		return (start_threads(recalc, g));
+	}
+	else if ((kk == 17 || kk == 15) && g->light_switch > 0 && g->light_switch <= g->lights)
+	{
+		g->liz[g->light_switch - 1] = g->liz[g->light_switch - 1] + 15 * (2 * (kk == 15) - 1);
+//		*g->li = sum(*g->li, scale((2 * (kk == 15) - 1), *g->normal));
+//		g->liz = g->li->z;
+//		return (start_threads(recalc, g));	
+	}
+	else if (kk == 49)
+	{
+		g->light_switch = (g->light_switch + 1) % (2 + g->lights);
+		return (1);
+	}
+	return (start_threads(toimg, g));
+}
+
+int		move_phys(int keycode, t_global *g)
+{
+	if (keycode == 1 || keycode == 13)
+		g->obj[g->objn].ctr->z += 5 * (2 * (keycode == 13) - 1);
+	else if	(keycode == 126 || keycode == 125)
+		g->obj[g->objn].ctr->y += 5 * (2 * (keycode == 126) - 1);
+	else if	(keycode == 0)
+		g->obj[g->objn].ctr->x += -5;
+	else if	(keycode == 2)
+		g->obj[g->objn].ctr->x += 5;
+	else if (keycode == 34)
+		g->obj[g->objn].ang.x -= 0.05;
+	else if (keycode == 40)
+		g->obj[g->objn].ang.x += 0.05;
+	else if (keycode == 38)
+		g->obj[g->objn].ang.y -= 0.05;
+	else if (keycode == 37)
+		g->obj[g->objn].ang.y += 0.05;
+	else if (keycode == 43 || keycode == 124)
+		g->obj[g->objn].ang.z -= 0.05;
+	else if (keycode == 47 || keycode == 123)
+		g->obj[g->objn].ang.z += 0.05;
+	if (keycode == 34 || keycode == 40 || keycode == 38 || keycode == 37
+	|| keycode == 43 || keycode == 47 || keycode == 123 || keycode == 124)
+	{
+		g->obj[g->objn].base[0] = rotate(g->base[0], g->obj[g->objn].ang);
+		g->obj[g->objn].base[1] = rotate(g->base[1], g->obj[g->objn].ang);
+		g->obj[g->objn].base[2] = rotate(g->base[2], g->obj[g->objn].ang);
+//		printf("base is %f,%f,%f\n", g->obj[g->objn].base[0].x, g->obj[g->objn].base[1].y, g->obj[g->objn].base[2].z);
+	}
+	campos(g);	
+	return (start_threads(move, g));
+}
+
+int	move_obj(int kk, t_global *g)
+{
+	if (g->objn != 0)
+		return (move_phys(kk, g));
+	if (kk == 126 || kk == 125 || kk == 38 || kk == 37
+		|| kk == 0 || kk == 2 || kk == 123 || kk == 124)
+	{
+		if (kk == 125)
+			g->angle->x += 0.05;
+		else if (kk == 126)
+			g->angle->x -= 0.05;
+		else if (kk == 38)
+			g->angle->z -= 0.05;
+		else if (kk == 37)
+			g->angle->z += 0.05;
+		else if (kk == 0 || kk == 123)
+			g->angle->y -= 0.05;
+		else if (kk == 2 || kk == 124)
+			g->angle->y += 0.05;
+		*g->normal = rotate(g->_0015, *g->angle);
+		campos(g);
+		return (start_threads(recalc, g));
+	}
+	else if (kk == 1)
+		*g->cam_pos = diff(*g->cam_pos, *g->normal);
+	else if (kk == 13)
+		*g->cam_pos = sum(*g->cam_pos, *g->normal);
+	campos(g);
+	return (start_threads(move, g));
+}
+
+void		move_cam(char s, t_global *g)
+{
+	int i;
+
+	i = 0;
+	while (i < g->argc)
+	{
+		if (s == '+')
+			*g->obj[i].ctr = sum(*g->obj[i].ctr, *g->normal);
+		else
+			*g->obj[i].ctr = diff(*g->obj[i].ctr, *g->normal);
+		i++;
+	}
+}
+
+//extern t_fun_and_arg *user_pointer;
+
+int		mouse_move(int x, int y, void *param)
+{
+	printf("address inside mouse move %p\n", param);	
+
+
+	t_global *g;
+	int	i;
+	t_vector p;
+	double block;
+
+	i = -1;
+	g = param;
+//	g = (t_global *)(user_pointer->param);
+/*
+	printf("address inside mouse move %p\n", param);	
+	printf("address user_pointer->param inside mouse move %p\n", user_pointer->param);
+	printf("address of g inside mouse move %p\n", g);
+
+	printf("inside mouse move\n");
+	printf("light switch %d\n", g->light_switch);
+*/
+//	g->light_switch = 1;
+
+	mousex = x;
+//	printf("shit is %f,%f\n", shot.x, shot.y);
+
+	if (g->light_switch >= 1 && g->light_switch <= g->lights) //move light
+	{
+			printf("changing t_global *g\n");
+			printf("light before is %f,%f,%f\n", g->li->x, g->li->y, g->li->z); 
+
+//			ft_bzero((int *)g->data_ptr, g->sz_l * HEIGHT);
+			block = g->liz[g->light_switch - 1] / g->ray->z;
+			p.x = (x - WIDTH_2) * block;
+			p.y = (HEIGHT_2 - y) * block;
+			p.z = g->liz[g->light_switch - 1];
+			printf("p is %f,%f,%f\n", p.x, p.y, p.z); 	
+			g->li[g->light_switch - 1] = sum(*g->cam_pos, rotate(p, *g->angle));
+			printf("light after is %f,%f,%f\n", g->li->x, g->li->y, g->li->z); 
+			start_threads(toimg, g);
+	}
+	else if (g->light_switch > g->lights) //move cam
+	{
+//		ft_bzero((int *)g->data_ptr, g->sz_l * HEIGHT);
+		p.y = /*sin*/((x - WIDTH_2)* 0.0045);
+		p.x = /*sin*/(0.0045 * (y - HEIGHT_2));
+		p.z = 0;
+//		*g->normal = rotate(g->_0015, p);
+//		*g->angle = sum(p, *g->angle);
+		*g->angle = p;
+
+//		ideally normal changes only when the key is pressed
+//		and doesn't change untill mouse_move->key_press
+//		muse move should set int can_change to 1
+//		key press should change normal and
+//		set int can change to 0
+
+		*g->normal = rotate(g->_0015, p);	
+
+		start_threads(recalc, g);
+	}
+	return (1);
+}
+
